cde4ca022da96966adbf56526f1a787e8e9bee48
TJHL <league@iMac.attlocal.net>
2017-11-18 11:27:07 -0800

    did web ting

--- copyrightFooter.js
+++ copyrightFooter.js
@@ -1,5 +1,6 @@
 function copyright() {
 var footer;
 footer = document.getElementById("copyright");
-footer.innerHTML += "Copyright <a href=\"http://www.jointheleague.org\">League of Amazing Programmers</a> 2013-2017";
+footer.innerHTML += "Copyright <a style=\"color: white;\" href=\"http://www.jointheleague.org\">League of Amazing Programmers</a> 2013-2017";
+console.log(footer.style);
 }
